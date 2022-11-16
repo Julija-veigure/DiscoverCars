ca1b4440dfafb58f8ed7d3fc709135b717def109
JulijaV <julija.veigure@gmail.com>
2022-11-16 01:16:57 -0800

    Created new page for the next checking

---
+++ src/test/java/pages/SearchingResultPage.java
@@ -0,0 +1,19 @@
+package pages;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class SearchingResultPage {
+    private final Logger LOGGER = (Logger) LogManager.getLogger(this.getClass());
+    private final BaseFunctions baseFunctions;
+
+
+    public SearchingResultPage(BaseFunctions baseFunctions) {
+        this.baseFunctions = baseFunctions;
+    }
+
+
+
+
+
+}
